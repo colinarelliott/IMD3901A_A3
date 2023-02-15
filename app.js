ed09af1e4f39fc4536a9d2c5ee71a41d9eaaea3f
colinarelliott <colinarelliott@gmail.com>
2023-02-15 18:29:36 -0500

    built app.js with sockets

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { SocketAddress } = require('net');
+const server = http.createServer(app);
+//SOCKET.IO
+const io = require('socket.io')(server);
+
+const LISTEN_PORT = 8080;
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {root:__dirname+'/public/'});
+});
+
+//socket.io samples
+io.on('connection', (socket) => {
+    console.log(socket.id + " is connected.");
+
+    socket.on('disconnect', () => {
+        console.log(socket.id + " has disconnected.");
+    });
+});
+
+app.use(express.static(__dirname + '/public'));
+server.listen(LISTEN_PORT);
+console.log('Listening to port ' + LISTEN_PORT);
