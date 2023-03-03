354738931be7365ca9b2522d6ba662310eb393e1
Colin Elliott <colinarelliott@gmail.com>
2023-03-03 15:59:15 -0500

    renamed pickup-container to pickupContainer

--- public/components/pickupContainer.js
+++ public/components/pickupContainer.js
@@ -1,6 +1,6 @@
 // a component that allows the crane magnets to pickup the nearest container when dropped.
 
-AFRAME.registerComponent('pickup-container', { //require the crane-controller component
+AFRAME.registerComponent('pickupContainer', { //dependent on the crane-controller component
     dependencies: ['crane-controller'],
     schema: {
         //list of all containers
@@ -8,23 +8,22 @@ AFRAME.registerComponent('pickup-container', { //require the crane-controller co
     },
     init: function () {
         const CONTEXT = this;
-        CONTEXT.onPickupContainer = CONTEXT.onPickupContainer.bind(CONTEXT);
-        //get the crane controller component
-        let craneController = document.querySelector('#craneController').getAttribute('crane-controller');
-        craneController.addEventListener('pickupContainer', CONTEXT.onPickupContainer);
+        CONTEXT.pickup = CONTEXT.pickup.bind(CONTEXT);
     },
 
-    onPickupContainer: function (eventData) {
+    pickup: function (data) {
+        const CONTEXT = this;
         console.log("pickupContainer function called");
         //refresh container list
         CONTEXT.data.containers = document.querySelectorAll('.shippingContainer');
         //add a list for the distances between the crane and the containers
         let distances = [];
         //set the magnet number
-        let magnetNumber = eventData;
+        let magnetNumber = data.craneToControl;
         console.log(magnetNumber);
         console.log('pickupContainer event received ' + magnetNumber);
         //get the crane controller component and reparent the closest container to the magnet
+
         let crane = document.querySelector('#crane' + magnetNumber);
         let cranePosition = crane.getAttribute('position');
         for (i = 0; i < CONTEXT.data.containers.length; i++) { //measure each distance between the containers and the crane
@@ -34,7 +33,13 @@ AFRAME.registerComponent('pickup-container', { //require the crane-controller co
             distances.push(Math.sqrt(Math.pow((containerPosition.x - cranePosition.x), 2) + Math.pow((containerPosition.z - cranePosition.z), 2)));
         }
         let containerToPickup = CONTEXT.data.containers[distances.indexOf(Math.min(distances))].getAttribute('id'); //get the index of the minimum distance
-        pickupContainer(magnetNumber, containerToPickup); //call the pickupContainer function in crane-controller.js
+        let container = document.querySelector('#' + containerToPickup);
+        let magnet = document.querySelector('#magnet' + magnetNumber);
+        magnet.append(container);
+        //set the container's position to 0,0,0
+        container.setAttribute('position', {x: 0, y: 0, z: 0});
+        //set the container's rotation to 0,0,0
+        container.setAttribute('rotation', {x: 0, y: 0, z: 0});
     },
 
     tick: function () {
