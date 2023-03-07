8ebcd62d7bd073e33316c3e99eab975472c70ffa
Colin Elliott <colinarelliott@gmail.com>
2023-03-07 12:34:36 -0500

    clarity of pickup / putdown decision

--- public/components/crane-controller.js
+++ public/components/crane-controller.js
@@ -75,14 +75,14 @@ AFRAME.registerComponent('crane-controller', {
             console.log(data);
 
             //if the event is from crane 1 and this player is controlling crane 2, pickup the container with the other crane
-            if (data.magnetNumber === 1 && CONTEXT.data.craneToControl === 2 && gameManager.data.crane1PickupAllowed === true) {
+            if (data.magnetNumber === 1 && CONTEXT.data.craneToControl === 2) {
                 pickupContainer.pickupSpecified( {
                     craneNum: data.magnetNumber,
                     containerID: data.containerToPickup,
                 });
             }
             //if the event is from crane 2 and this player is controlling crane 1, pickup the container with the other crane
-            if (data.magnetNumber === 2 && CONTEXT.data.craneToControl === 1 && gameManager.data.crane2PickupAllowed === true) {
+            if (data.magnetNumber === 2 && CONTEXT.data.craneToControl === 1) {
                 pickupContainer.pickupSpecified( {
                     craneNum: data.magnetNumber,
                     containerID: data.containerToPickup,
@@ -309,15 +309,16 @@ AFRAME.registerComponent('crane-controller', {
     onKeyup: function(evt) {
         const CONTEXT = this;
         const pickupContainer = document.querySelector('[pickupContainer]').components.pickupContainer;
-        const gameManager = document.querySelector('[game-manager]').components.gameManager;
+        const gameManager = document.querySelector('#gameManager').components['game-manager'];
         switch(evt.keyCode) { 
             case 32: //SPACE
                 //animate the magnet back up to 82
                 CONTEXT.data.magnetPosY = 82;
                 //call pickup or putdown function depending on which is allowed and conditional to the craneToControl
-                if (gameManager.data.crane1PickupAllowed && CONTEXT.data.craneToControl === 1 || gameManager.data.crane2PickupAllowed && CONTEXT.data.craneToControl === 2) {
+                if (gameManager.data.crane1PickupAllowed === true && CONTEXT.data.craneToControl === 1 || gameManager.data.crane2PickupAllowed === true && CONTEXT.data.craneToControl === 2) {
                     pickupContainer.pickup(CONTEXT.data);
-                } else if (gameManager.data.crane1PutdownAllowed && CONTEXT.data.craneToControl === 1 || gameManager.data.crane2PutdownAllowed && CONTEXT.data.craneToControl === 2) {
+                }
+                if (gameManager.data.crane1PutdownAllowed === true && CONTEXT.data.craneToControl === 1 || gameManager.data.crane2PutdownAllowed === true && CONTEXT.data.craneToControl === 2) {
                     pickupContainer.putdown(CONTEXT.data);
                 }
                 break;
