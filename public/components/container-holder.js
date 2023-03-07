814c88491523baf19092f4e829195dfd8e2cc914
Colin Elliott <colinarelliott@gmail.com>
2023-03-07 11:27:57 -0500

    simplified this back down to an integer value

--- public/components/container-holder.js
+++ public/components/container-holder.js
@@ -22,16 +22,6 @@ AFRAME.registerComponent('container-holder', {
 
     tick: function () {
         const CONTEXT = this;
-        /*
-        //get all the children of the cargo ship
-        let children = CONTEXT.el.children;
-        //loop through the children
-        for (let i = 0; i < children.length; i++) {
-            //show the containers on the ship
-            children[i].setAttribute('visible', true);
-            //set the position of the containers on the ship
-            children[i].setAttribute('position', {x: 0, y: 0.5, z: 0});
-        }*/
     },
 
     updateServer: function () {
@@ -44,31 +34,14 @@ AFRAME.registerComponent('container-holder', {
         });
     },
 
-    addContainer: function (container) {
+    addContainer: function () {
         const CONTEXT = this;
         console.log("container added to "+ CONTEXT.el.id);
         //add one to the containerCount
         CONTEXT.data.containerCount++;
-        console.log("container added, total: " + CONTEXT.data.containerCount);
-
-        let containerID = container.getAttribute('id');
-
-        let containerToAdd = document.querySelector('#'+containerID);
-        let copy = containerToAdd.cloneNode(true);
-        setTimeout(function () {
-            copy.setAttribute('class', 'containerOnShip');
-            copy.setAttribute('scale', {x: 1, y: 1, z: 1});
-            copy.setAttribute('position', {x: 0, y: 5, z: 0});
-            copy.setAttribute('rotation', {x: 0, y: 0, z: 0});
-            copy.setAttribute('gltf-model', 'assets/models/blue-container.gltf');
-            //append the incoming container to the cargo ship
-            CONTEXT.el.appendChild(copy);
-            //remove the incoming container
-            containerToAdd.parentNode.removeChild(containerToAdd);
-        }, 10);
     },
 
-    removeContainer: function (containerID) {
+    removeContainer: function () {
         const CONTEXT = this;
         CONTEXT.data.containerCount--;
         console.log("container removed, total: " + CONTEXT.data.containerCount);
